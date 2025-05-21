2357343151829382a7bbe50b0bba3c71cf712306
lazarodu <lazarodu@gmail.com>
2025-05-21 12:36:25 -0300

    testes react mock

--- tests/UserListAxios.test.tsx
+++ tests/UserListAxios.test.tsx
@@ -3,11 +3,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import UserListAxios from '../src/components/UserListAxios';
 import axios from 'axios';
 vi.mock('axios');
-const mockedAxios = vi.mocked(axios);
+const mockedAxios = vi.mocked(axios.get);
 
 describe('UserListAxios', () => {
   it('deve exibir usuários após o carregamento', async () => {
-    mockedAxios.get.mockResolvedValueOnce({
+    mockedAxios.mockResolvedValueOnce({
       data: [
         { id: 1, name: 'Carla' },
         { id: 2, name: 'Diego' }
